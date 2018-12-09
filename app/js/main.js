4dae7400915e8eb8dfb6df06b56e132fe9b51422
Ptashkina Liliya <efekta9@gmail.com>
2018-12-09 13:32:24 +0300

    no message

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,39 @@
-ymaps.ready(init);
- function init(){     
-    var myMap1 = new ymaps.Map("map_contacts", {
-         center: [47.22261057425376,39.61439249999996],
-         zoom: 17
-     }),
-     
-     // Создаем метку с помощью вспомогательного класса.
-     myPlacemark1 = new ymaps.Placemark([47.22261057425376,39.61439249999996], {
-      // Свойства.
-      // Содержимое иконки, балуна и хинта.
-      balloonContentBody: 'Транспортная компания Вигард',
-      balloonContentFooter: 'Россия, Ростов-на-Дону, улица Малиновского, 54Г, офис 204',
-      hintContent: 'Транспортная компания Вигард'
-  }, {
-      // Опции.
-      // Своё изображение иконки метки.
-      iconLayout: 'default#imageWithContent',
-      iconImageHref: 'img/marker.png',
-      // Размеры метки.
-      iconImageSize: [30, 50]
-  });
+$(document).ready(function() {
+  // map
+  ymaps.ready(init);
+     function init(){     
+        var myMap1 = new ymaps.Map("map", {
+             center: [47.22261057425376,39.61439249999996],
+             zoom: 17
+         }),
+         
+         // Создаем метку с помощью вспомогательного класса.
+         myPlacemark1 = new ymaps.Placemark([47.22261057425376,39.61439249999996], {
+          // Свойства.
+          // Содержимое иконки, балуна и хинта.
+          balloonContentBody: 'Транспортная компания Вигард',
+          balloonContentFooter: 'Россия, Ростов-на-Дону, улица Малиновского, 54Г, офис 204',
+          hintContent: 'Транспортная компания Вигард'
+      }, {
+          // Опции.
+          // Своё изображение иконки метки.
+          iconLayout: 'default#imageWithContent',
+          iconImageHref: 'img/marker.png',
+          // Размеры метки.
+          iconImageSize: [30, 50]
+      });
 
-// Добавляем все метки на карту.
-myMap1.geoObjects.add(myPlacemark1);
-myMap1.behaviors.disable('scrollZoom');
+    // Добавляем все метки на карту.
+    myMap1.geoObjects.add(myPlacemark1);
+    myMap1.behaviors.disable('scrollZoom');
 
- };
+     };
 
- 
+
+//Tabs
+    $(".tab_item").not(":first").hide();
+    $(".wrapper-tab .tab").click(function() {
+      $(".wrapper-tab .tab").removeClass("tab-active").eq($(this).index()).addClass("tab-active");
+      $(".tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("tab-active");
+});
