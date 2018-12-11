7d97bf7c66300b553b1bf23599ff3170c7538596
Ptashkina Liliya <efekta9@gmail.com>
2018-12-11 17:20:33 +0300

    no message

--- app/js/main.js
+++ app/js/main.js
@@ -56,20 +56,11 @@ $(document).ready(function() {
 //     );
 // // else ()
 // }
-  var contents = $('.accordeon-content');
-  var titles = $('.accordeon-title');
-    titles.on('click',function(){
-      var title = $(this);
-      contents.filter(':visible').slideUp(function(){
-        $(this).prev('.accordeon-title').removeClass('is-opened');
-      });  
-      
-      var content = title.next('.accordeon-content'); 
-      
-      if (!content.is(':visible')) {
-        content.slideDown(function(){title.addClass('is-opened')});
-      } 
-    });
+ $('.js-open').on('click', function(e) {
+    // e.preventDefault();
+    $('.js-hide').toggleClass('js-show');
+})
+
 });
 
 
