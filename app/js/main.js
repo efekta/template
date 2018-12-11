22c2b9d6efab5e7fba8b36ab1ff81e4842ececc3
Ptashkina Liliya <efekta9@gmail.com>
2018-12-11 16:46:38 +0300

    icon modal

--- app/js/main.js
+++ app/js/main.js
@@ -56,7 +56,20 @@ $(document).ready(function() {
 //     );
 // // else ()
 // }
-
+  var contents = $('.accordeon-content');
+  var titles = $('.accordeon-title');
+    titles.on('click',function(){
+      var title = $(this);
+      contents.filter(':visible').slideUp(function(){
+        $(this).prev('.accordeon-title').removeClass('is-opened');
+      });  
+      
+      var content = title.next('.accordeon-content'); 
+      
+      if (!content.is(':visible')) {
+        content.slideDown(function(){title.addClass('is-opened')});
+      } 
+    });
 });
 
 
