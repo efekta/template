b96b6d44111ca6d3de90d750100712e27f0d92ca
Ptashkina Liliya <efekta9@gmail.com>
2018-12-11 17:46:03 +0300

    no message

--- app/js/main.js
+++ app/js/main.js
@@ -42,24 +42,17 @@ $(document).ready(function() {
 
 //Table Mobile
 // if (window.innerWidth < 728) { 
-//     $(".contacts-page-table-wrapp table").replaceWith( $(".contacts-page-table-wrapp table").html()
-//        .replace(/<thead/gi, "<div")
-//        .replace(/<th/gi, "<div")
-//        .replace(/<tbody/gi, "<div")
-//        .replace(/<tr/gi, "<div")
-//        .replace(/<\/tr>/gi, "</div>")
-//        .replace(/<td/gi, "<div")
-//        .replace(/<\/td>/gi, "</div")
-//        .replace(/<\/th/gi, "<\/div")
-//        .replace(/<\/thead/gi, "<\/div")
-//        .replace(/<\/tbody/gi, "<\/div")
-//     );
-// // else ()
+//   document.querySelectorAll('.contacts-page-table-wrapp > table')
 // }
+
+
+
+//Contacts Page Table
  $('.js-open').on('click', function(e) {
     // e.preventDefault();
     $('.js-hide').toggleClass('js-show');
 })
+
 
 });
 
